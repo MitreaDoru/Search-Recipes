f7bc34fe2d39b739efe8f1ee302289f1df5cff3e
MitreaDoru <mitrea.doru.catalin@gmail.com>
2022-10-06 15:33:30 +0300

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,9 +130,7 @@ const controlAddRecipe = async function (newRecipe) {
 
   // Upload the new recipe data
 };
-const newFeature = function () {
-  console.log('Welcome to the app');
-};
+
 const init = function () {
   bookmarksView.addHendlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -141,6 +139,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
