c264a174825aba72a790ea8208e560c1ea8cb224
MitreaDoru <mitrea.doru.catalin@gmail.com>
2022-10-06 11:39:51 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43f6c32a-bfac-451c-8e6d-d2ec9a6ee9dd';
+export const MODAL_CLOSE_SEC = 2.5;
